2442c26595b357fc750180e33482325c55bde359
nigel.thorne <nigel.thorne@2d40de48-a045-0410-863a-abe81852e6d9>
2008-05-01 02:14:40 +0000

    making broadcaster safe for changing the list while handling the event.  : Adding test

--- src/Tests/TypeSafeBroadcasterTests.cs
+++ src/Tests/TypeSafeBroadcasterTests.cs
@@ -1,6 +1,7 @@
 //Copyright (c) 2008 Nigel Thorne
 using System;
 using NDependencyInjection.Generics;
+using NDependencyInjection.interfaces;
 using NMock2;
 using NMockExtensions;
 using NUnit.Framework;
@@ -101,6 +102,34 @@ namespace NDependencyInjection.Tests
             broadcaster.AddListeners(new IBroadcasterTestsListener[] { listener1, listener2 });
             broadcaster.RemoveListeners(new IBroadcasterTestsListener[] { listener1, listener2 });
             broadcaster.Listener.OnBanana(1, 2);
+        }
+
+        [Test]
+        public void RemoveListener_RemovesListener_EvenWhenHandlingTheEvent()
+        {   
+            broadcaster = new TypeSafeBroadcaster<IBroadcasterTestsListener>();
+            broadcaster.AddListeners(new ForgetMeListener(broadcaster) );
+            broadcaster.Listener.OnBanana(1, 2);
+        }
+    }
+
+    internal class ForgetMeListener : IBroadcasterTestsListener
+    {
+        private readonly IBroadcaster<IBroadcasterTestsListener> broadcaster;
+
+        public ForgetMeListener(IBroadcaster<IBroadcasterTestsListener> broadcaster)
+        {
+            this.broadcaster = broadcaster;
+        }
+
+        public void OnBanana(int x, int y)
+        {
+            broadcaster.RemoveListeners(this);
+        }
+
+        public int GetSomething()
+        {
+            return 0;
         }
     }
 }
