3a3e7c2b53b9b2d047313dff1e8b3d5dd11ea23c
nigel.thorne <nigel.thorne@2d40de48-a045-0410-863a-abe81852e6d9>
2008-08-12 03:34:30 +0000

    Visualizer has colours

--- trunk/NDIVisualizer/NDIGraph/Controls/GraphControl.cs
+++ trunk/NDIVisualizer/NDIGraph/Controls/GraphControl.cs
@@ -1,48 +1,43 @@
+using System;
+using System.Windows.Forms;
+using Microsoft.Glee.Drawing;
 using Microsoft.Glee.GraphViewerGdi;
-
 
 namespace Reflector.NDIGraph
 {
-	using System.Collections;
-	using System.Collections.Specialized;
-	using System.Drawing;
-	using System.IO;
-	using System.Windows.Forms;
-	using System;
-	using System.ComponentModel;
-	using Reflector.CodeModel;
-	using System.Reflection;
-	using System.Xml;
-	using Microsoft.Glee.Drawing;
-
-	internal abstract class GraphControl : UserControl
+    internal abstract class GraphControl : UserControl
     {
-        private IAssemblyBrowser assemblyBrowser = null;
-        private Microsoft.Glee.GraphViewerGdi.GViewer viewer;
+        private readonly IAssemblyBrowser assemblyBrowser;
+        private readonly GViewer viewer;
 
-		public GraphControl(IServiceProvider serviceProvider)
+        public GraphControl(IServiceProvider serviceProvider)
         {
-			this.assemblyBrowser = (IAssemblyBrowser)serviceProvider.GetService(typeof(IAssemblyBrowser));
+            assemblyBrowser = (IAssemblyBrowser) serviceProvider.GetService(typeof (IAssemblyBrowser));
 
-            this.viewer = new Microsoft.Glee.GraphViewerGdi.GViewer();
-            this.viewer.SelectionChanged += new EventHandler(OnSelectionChanged);
-            DockControl(this.viewer);
+            viewer = new GViewer();
+            viewer.SelectionChanged += OnSelectionChanged;
+            DockControl(viewer);
         }
 
-	    public void DockControl(Control control)
-	    {
-	        control.Dock = DockStyle.Fill;
-	        this.Controls.Add(control);
-	        this.Dock = DockStyle.Fill;
-	    }
-
-	    protected Microsoft.Glee.Drawing.Graph CreateGraph(string label)
+        public GViewer Viewer
         {
-            Microsoft.Glee.Drawing.Graph graph = new Microsoft.Glee.Drawing.Graph(label);
+            get { return viewer; }
+        }
+
+        public void DockControl(Control control)
+        {
+            control.Dock = DockStyle.Fill;
+            Controls.Add(control);
+            Dock = DockStyle.Fill;
+        }
+
+        protected Graph CreateGraph(string label)
+        {
+            Graph graph = new Graph(label);
             graph.GraphAttr.NodeAttr.FontName = "Tahoma";
             graph.GraphAttr.NodeAttr.Fontsize = 8;
             graph.GraphAttr.NodeAttr.Shape = Shape.Box;
-            graph.GraphAttr.NodeAttr.Fillcolor = Microsoft.Glee.Drawing.Color.WhiteSmoke;
+            graph.GraphAttr.NodeAttr.Fillcolor = Color.WhiteSmoke;
 
             graph.GraphAttr.EdgeAttr.FontName = "Tahoma";
             graph.GraphAttr.EdgeAttr.Fontsize = 8;
@@ -51,31 +46,26 @@ namespace Reflector.NDIGraph
 
         protected virtual void OnSelectionChanged(object sender, EventArgs e)
         {
-            if (this.viewer.SelectedObject == null)
+            if (viewer.SelectedObject == null)
                 return;
 
-            Node node = this.viewer.SelectedObject as Node;
+            Node node = viewer.SelectedObject as Node;
             if (node != null)
             {
                 object value = node.UserData;
                 if (value != null)
-					this.assemblyBrowser.ActiveItem = value;
+                    assemblyBrowser.ActiveItem = value;
                 return;
             }
 
-            Microsoft.Glee.Drawing.Edge edge = this.viewer.SelectedObject as Microsoft.Glee.Drawing.Edge;
+            Edge edge = viewer.SelectedObject as Edge;
             if (edge != null)
             {
                 object value = edge.UserData;
                 if (value != null)
-					this.assemblyBrowser.ActiveItem = value;
+                    assemblyBrowser.ActiveItem = value;
                 return;
             }
-        }
-
-        public Microsoft.Glee.GraphViewerGdi.GViewer Viewer
-        {
-            get { return this.viewer; }
         }
     }
 }
